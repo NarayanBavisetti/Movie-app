245086e55fec226806f9b653636047e4d52db45f
narayan <narayan3119@gmail.com>
2021-07-30 23:07:15 +0530

    final deployment

--- Backend/index.js
+++ Backend/index.js
@@ -21,12 +21,12 @@ app.get("/",(req,res) => {
     res.send("HI bruh");
 })
 
-if ( process.env.NODE_ENV == "production"){
-    app.use(express.static("client/build"));
-    // const path = require("path");
-    // app.get("*", (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    // })
+if ( process.env.NODE_ENV === "production"){
+    app.use(express.static("frontend/build"));
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
 }
 
 app.listen(PORT,() => console.log(`PORT running at ${PORT}`))
